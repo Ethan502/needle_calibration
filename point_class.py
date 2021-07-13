f52c2e5f9625f1ae7536313e14e6e58b769d6c28
Ethan Smith <ethan.smith502@yahoo.com>
2021-07-13 13:26:02 -0600

    started the circle class

---
+++ point_class.py
@@ -0,0 +1,78 @@
+"""
+Calibration Class for interpreting the nanoPOTS chip images
+    Opens image passed in and finds the coordinates of the calibration point center
+To implement this class you need to:
+    - pass in an image and a needle mask
+    - set_roi_vertices(vertices)
+    - call self.find_point(img)
+    call these before self.find_point() if you want to change settings
+        default thresh = (60, 200)
+        self.thresh_l = num # initialize lower threshold
+        self.thresh_u =  #initialize upper threshold
+"""
+
+import cv2 as cv
+import numpy as np
+
+class PointFinder:
+    # Constructor ---------------------------------------------------------
+    def __init__(self, frame):
+        # Data members ----------------------------------------------------
+        self.img = frame
+        self.center = [] # x,y coordinates of the calibration point center
+        #outline of the needle, also useful for finding the needle location if it moves
+        self.needle_vertices = []
+        # [(x,y), (x,y), etc] where x=0 is left and y=0 is top
+        self.roi_vertices = []
+        self.needle_vertices = []
+        self.c_area =[] # gives area of the contour found
+        # default/settings
+        # threshold filter settings
+        self.thresh_l = 60
+        self.thresh_u = 250
+        self.flag_roi_in_out = 0
+        # area filter settings
+        self.min_area = 1300
+        self.max_area = 2500
+        # images during different stages of image processing
+        self.gray_img = []
+        self.blurred_img = []
+        self.cropped_img = []
+        self.thresh_img = []
+        self.filtered_mask_img = []
+
+    #Filters ---------------------------------------------------------------
+
+    # ROI, Masks inside or outside of the shape given. Mask Outside shape is flag_roi_in_out = 0
+    # Mask shape if flag_roi_in_out = 1
+    # working with black and white images, so don't need to specify channel count
+    def region_of_interest(self, img, vertices):
+        #this process keeps pixels inside shape given
+        if (self.flag_roi_in_out == 0):
+            mask = np.zeros_like(img)
+            match_mask_color = (255,255,255)
+            cv.fillPoly(mask, vertices, match_mask_color)
+            cv.bitwise_not(mask)
+            masked_image = cv.bitwise_and(img.copy(), mask)
+        else:
+            # this process masks pixels inside shape given
+            mask = cv.bitwise_not(np.zeros_like(img))
+            match_mask_color = (0,0,0)
+            cv.fillPoly(mask, vertices, match_mask_color)
+            masked_image = cv.bitwise_and(img, mask)
+        return masked_image
+
+
+    # Find point ------------------------------------------------------------
+    # implements the class
+
+    def process(self):
+        # convert img to grayscale and blur
+        self.gray_img = cv.cvtColor(self.img.copy(), cv.COLOR_BGR2GRAY)
+        self.blurred_img = cv.medianBlur(self.gray_img.copy(), 9)
+        # crop out edges of frame that confuse the cv
+        self.cropped_img = self.region_of_interest(self.blurred_img, np.array([self.roi_vertices], np.int32))
+        cv.imshow('window', self.cropped_img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
--- tester.py
+++ tester.py
@@ -2,8 +2,12 @@ import cv2 as cv
 import numpy as np
 
 from needle_class import NeedleBoy
+from point_class import PointFinder
 
 pic = cv.imread('static/images/needle1.jpg')
 
-tester = NeedleBoy(pic)
+# tester = NeedleBoy(pic)
+# tester.process()
+
+tester = PointFinder(pic)
 tester.process()
