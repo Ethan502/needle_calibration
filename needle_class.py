6504a3eb51885ccbdffffc5856e51d46048f44cf
Ethan Smith <ethan.smith502@yahoo.com>
2021-08-12 12:37:57 -0600

    more touch ups, function descriptions

--- needle_class.py
+++ needle_class.py
@@ -34,6 +34,12 @@ class NeedleBoy():
         self.needle_count = 0
 
     def filter(self,img_):
+        """[recieved the black and white image from the contouring function, then filters out the contours not in the needed area]
+        Args:
+            img_ ([numpy.ndarray]): [b/w image of a bunch of contours]
+        Returns:
+            [numpy.ndarray]: [returns one or two masks with a needle contour on each, depending on how many contours are detected]
+        """
         contours, _ = cv.findContours(img_.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         needle_contours = []
         frame1 = np.zeros_like(img_)
@@ -43,10 +49,10 @@ class NeedleBoy():
             c_area = cv.contourArea(c)
             # print(i) 
             # print(c_area)
-            if 10000 <= c_area <= 400000:
+            if 10000 <= c_area <= 40000:
                 needle_contours.append(c)
-        self.needle_count = len(needle_contours)
-        print(f"The number of contours is: {self.needle_count}")
+        self.needle_count = len(needle_contours) # variable to determine how many needles are found and how many masks are made
+        print(f"The number of needle contours is: {self.needle_count}")
         # only draws and the contours for the number of contour areas detected
         if self.needle_count == 1:
             cv.drawContours(frame1, needle_contours, 0, 255, cv.FILLED)
@@ -63,6 +69,12 @@ class NeedleBoy():
 
 
     def contour_maker(self,pic): 
+        """function will take the picture, and go through a contouring process to find the ends of the needle then create a mask 
+        Args:
+            pic ([numpy.ndarray]): [the original picture from cv.imread]
+        Returns:
+            [numpy.ndarray]: [will change the picture into the correct number of masks based off the found number of needle contours]
+        """
         blur = cv.medianBlur(pic,5)
         lab = cv.cvtColor(blur, cv.COLOR_BGR2LAB)
         lab_planes = cv.split(lab)
@@ -86,10 +98,8 @@ class NeedleBoy():
 
     def region_of_interest(self, vertices):
         """creates a mask on a grayscale img_ for vertices given
-
         Args:
             vertices (list): [points for where to put the mask]
-
         Returns:
             np.ndarray: [grayscale image with vertices mask placed]
         """
@@ -102,7 +112,6 @@ class NeedleBoy():
     def needle_extremes_right(self, _img):
         """finds the left, bottom, and topmost points of the rightmost contour in grayscale
         img that only has the needles in it.
-
         Args:
             _img (numpy.ndarray): [blurred b/w img that has the contoured needle]
         """
@@ -118,7 +127,6 @@ class NeedleBoy():
 
     def needle_extremes_left(self, _img):
         """finds the right, bottom, and top most points for the leftmost contour of the image.
-
         Args:
             _img ([numpy.ndarray]): [blurred b/w image of just the left contour]
         """
@@ -136,13 +144,11 @@ class NeedleBoy():
         """
         Args:
             img_ ([numpy.ndarray]): [image you want to grab]
-
         Returns:
             [numpy.ndarray]: [the needle mask]
         """
         mask1, mask2 = self.contour_maker(img_)
-        #extend_mask calls needle_extremes which assigns the leftmost and rightmost points
-        
+        #calls needle_extremes which assigns the leftmost and rightmost points depending on the number of found contours
         if self.needle_count == 1:
             newmask1 = self.needle_extremes_right(mask1)
             return newmask1, None
