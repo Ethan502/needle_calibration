dd9d20d6b0a9369c827b34d1b459196a8fadcf1c
Ethan Smith <ethan.smith502@yahoo.com>
2021-08-17 17:57:38 -0600

    all the app stuff

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, redirect, url_for, render_template, request
+import os
+import cv2 as cv
+
+from finder_class import CalibrationPoints
+
+
+app = Flask(__name__)
+
+image_folder = os.path.join(os.getcwd(), 'static', 'images')
+processed_folder = os.path.join(os.getcwd(), 'processed')
+
+app.config["IMAGE_FOLDER"] = image_folder
+app.config["PROCESSED_FOLDER"] = processed_folder
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+
+    filelist = os.listdir(image_folder)
+
+    if request.method == 'POST':
+        original_image = os.path.join(app.config["IMAGE_FOLDER"], request.form["file1"])
+        img_data = cv.imread(original_image)
+        print(type(img_data))
+
+        points = CalibrationPoints(img_data)
+        points.process()
+        new_data = points.img
+
+        cv.imwrite(os.path.join(app.config["PROCESSED_FOLDER"], 'processed.jpg'), new_data)
+        new_pic = (os.path.join(app.config["PROCESSED_FOLDER"], 'processed.jpg'))
+        
+        return render_template("index.html", filelist=filelist, 
+                                len = len(filelist), final_og_pic=original_image,
+                                final_new_pic = new_pic)                           
+
+    return render_template("index.html", filelist = filelist, len = len(filelist))
+
+if __name__ == "__main__":
+    app.run(debug=True)
