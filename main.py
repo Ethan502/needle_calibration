94774e3b83fa55d6827b685edda93175be0cfd02
Ethan Smith <ethan.smith502@yahoo.com>
2021-09-13 12:25:43 -0600

    stuff

--- main.py
+++ main.py
@@ -10,6 +10,3 @@ right_needle_point = point.rightpoint
 left_needle_point = point.leftpoint
 calibration_point = point.calib_point
 
-print(f"This is the point on the left side {left_needle_point}")
-print(f"This is the point on the right side {right_needle_point}")
-print(f"This is the center circle point {calibration_point}")
