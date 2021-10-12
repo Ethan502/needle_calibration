782738c141075b8e36aead2830b0ef9269ba1e17
Ethan502 <ethan.smith502@yahoo.com>
2021-10-12 12:00:44 -0600

    updated motor series

--- motor_series.py
+++ motor_series.py
@@ -4,6 +4,7 @@ MOTOR SERIES CLASS
     all the native functionalities in a coordinated manner
 """
 
+from _typeshed import Self
 from zaber_motion import Library, MotionLibException
 from zaber_motion.ascii import Connection
 from motor import Motor
@@ -25,8 +26,23 @@ class MotorSeries:
     def __init__(self, com_port, config_file, log_file_name_head, logging_level=logging.DEBUG):
         
         # Zaber factory logs ---------------------------------------------
-            file_name_tail = "_zaber_log.log"
-            log_file_path = log_file_name_head + file_name_tail
-            Library.set_log_output(LogOutputMode.FILE, log_file_path)
+        file_name_tail = "_zaber_log.log"
+        log_file_path = log_file_name_head + file_name_tail
+        Library.set_log_output(LogOutputMode.FILE, log_file_path)
         #-----------------------------------------------------------------
+        
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging_level)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
+        file_handler = logging.FileHandler(f'{log_file_name_head}_{__name__}_log.log', mode = "w")
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(stream_handler)
+
+        
+
+        
